092c933309ba91ea30ff19ac22cae30add18fbba
Nils Knappmeier <n.knappmeier@i-views.de>
2015-06-29 22:27:34 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * thought <https://github.com/nknapp/thought>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function thought () {
+  // body
+}
---
+++ spec/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * thought <https://github.com/nknapp/thought>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var thought = require('../')
+
+describe('thought:', function () {
+  // body
+})
