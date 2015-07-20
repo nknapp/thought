e006f5fbf9520c5948bc514fbae4b951a1d5f807
Nils Knappmeier <n.knappmeier@i-views.de>
2015-07-21 00:03:00 +0200

    Add "htmlId" helper

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -235,6 +235,15 @@ module.exports = {
   npm: function (packageName) {
     return '[' + packageName + '](https://npmjs.com/package/' + packageName + ')'
 
+  },
+
+  /**
+   * Replace all characters that may not be used in HTML id-attributes by '-'.
+   * There is still the restriction that IDs may only start with letters, which
+   * is not addressed by this helper.
+   */
+  "htmlId": function(value) {
+    return value.replace(/[^A-Za-z0-9-_:.]/g,"");
   }
 }
 
