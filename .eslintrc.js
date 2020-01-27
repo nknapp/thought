55e9bff6fdc36080423e538170fe4da0fc41e7bb
Nils Knappmeier <github@knappi.org>
2020-01-27 19:01:25 +0100

    chore: bump dependencies, use prettier for formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,31 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ],
-    rules: {
-      "no-console": "warn"
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ['standard', 'prettier'],
+  plugins: ['promise'],
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "no-var": "error",
+    "prefer-const": "warn",
+    "no-console": "warn"
+  },
+  overrides: [
+    {
+      files: ['**/test/*-spec.js'],
+      env: {
+        mocha: true
+      }
+    },
+    {
+      files: ['examples/**/*.js'],
+      rules: {
+        'no-console': 'off'
+      }
     }
-};
+  ]
+}
