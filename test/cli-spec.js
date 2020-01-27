721f34d95ab6acfe36b138ce44214984e2cd9c06
Nils Knappmeier <github@knappi.org>
2020-01-27 21:27:55 +0100

    refactor: remove "check-engines"-option
    - The option is not required anymore, because
      we only support node@lts and that does not come
      with npm@2
    - bump commander to latest version
    - run actual "bin/thought" to test the CLI
    - configure nyc tresholds
    
    BREAKING CHANGE: "check-engines"-options removed

--- test/cli-spec.js
+++ test/cli-spec.js
@@ -3,29 +3,12 @@
 const Scenario = require('./lib/scenarios')
 const chai = require('chai')
 const expect = chai.expect
-const cli = require('../lib/cli.js')
 const exec = require('../lib/utils/exeq')
-const cpMock = require('./lib/child-process-mock')
+const cp = require('child_process')
 
 describe('The cli script', function() {
   this.timeout(30000)
   const scenario = new Scenario('simple-project')
-  const stackTraceLimit = Error.stackTraceLimit
-
-  beforeEach(function() {
-    // Make "npm install" faster by not really executing it
-    cpMock.mockSpawn(
-      cmd => cmd.match(/npm/),
-      function(cmd, args, options) {
-        setTimeout(() => this.emit('exit', 0), 10)
-      }
-    )
-  })
-
-  afterEach(() => {
-    cpMock.clearMocks()
-    Error.stackTraceLimit = stackTraceLimit
-  })
 
   it('should report missing hooks in package.json', function() {
     return scenario
@@ -45,32 +28,6 @@ describe('The cli script', function() {
         expect(result.code).to.equal(0)
         expect(result.stderr).to.equal('')
         expect(scenario.readActual('README.md')).to.equal(scenario.readExpected('README.md'))
-      })
-  })
-
-  it('should check engines with no errors in a simple project', function() {
-    return scenario
-      .prepareAndRun(() => runMockThought('check-engines'))
-      .then(result => {
-        expect(result.stdout).to.equal('OK')
-        expect(result.stderr).to.equal('')
-        expect(result.code).to.equal(0)
-      })
-  })
-
-  it('should fail the engine check if the npm-version does not match', function() {
-    cpMock.mockExecFile(
-      (cmd, args) => cmd.match(/npm/) && args && args[0] === '--version',
-      function(cmd, args, options, callback) {
-        callback(null, '2.0.0', '')
-      }
-    )
-    return scenario
-      .prepareAndRun(() => runMockThought('check-engines'))
-      .then(result => {
-        expect(result.code).to.equal(1)
-        expect(result.stdout).to.equal('')
-        expect(result.stderr).to.match(/will not execute the `version`-script/)
       })
   })
 
@@ -136,22 +93,13 @@ describe('The cli script', function() {
  */
 function runMockThought(...args) {
   return new Promise((resolve, reject) => {
-    const argv = ['node', require.resolve('../bin/thought')].concat(args)
-    const fakeConsole = {
-      stdout: [],
-      stderr: [],
-      error: function(...message) {
-        this.stderr.push(message.join(' '))
-      },
-      log: function(...message) {
-        this.stdout.push(message.join(' '))
+    return cp.execFile(
+      process.argv0,
+      ['-r', require.resolve('./lib/mock-npm-commands'), require.resolve('../bin/thought')].concat(args),
+      (error, stdout, stderr) => {
+        const code = error ? error.code : 0
+        resolve({ code, stderr, stdout })
       }
-    }
-    delete require.cache[require.resolve('commander')]
-    cli(argv, fakeConsole, code => {
-      const stdout = fakeConsole.stdout.join('\n')
-      const stderr = fakeConsole.stderr.join('\n')
-      resolve({ code, stdout, stderr })
-    })
+    )
   })
 }
---
+++ test/lib/mock-npm-commands.js
@@ -0,0 +1,9 @@
+const cpMock = require('./child-process-mock')
+
+// Make "npm install" faster by not really executing it
+cpMock.mockSpawn(
+  cmd => cmd.match(/npm/),
+  function(cmd, args, options) {
+    setTimeout(() => this.emit('exit', 0), 10)
+  }
+)
