8040572d3853cf3d53374a03d283c0d3277a3f43
Nils Knappmeier <github@knappi.org>
2020-07-03 01:11:18 +0200

    feat: add command "eject" to override default files in the current project

--- test/cli-spec.js
+++ test/cli-spec.js
@@ -6,6 +6,9 @@ const expect = chai.expect
 const exec = require('../lib/utils/exeq')
 const cp = require('child_process')
 
+const fs = require('fs-extra')
+const path = require('path')
+
 describe('The cli script', function() {
   this.timeout(30000)
   const scenario = new Scenario('simple-project')
@@ -83,6 +86,110 @@ describe('The cli script', function() {
           expect(result.stderr).to.equal('')
         })
     )
+  })
+
+  describe('the "eject" command', () => {
+    const ejectScenario = new Scenario('with-partial-and-template')
+
+    it('should show a list of ejectable files if no parameter is provided', async function() {
+      await ejectScenario.prepareAndRun(() =>
+        runMockThought('eject').then(result => {
+          expect(result.code).to.equal(0)
+          expect(result.stdout).to.match(/partial howitworks\.md\.hbs/)
+          expect(result.stdout).to.match(/template README\.md\.hbs/)
+          expect(result.stdout).not.to.match(/usage\.md\.hbs/)
+
+          expect(result.stderr).to.equal('')
+        })
+      )
+    })
+
+    it('should show only ejectable templates, if the prefix is "template"', async function() {
+      await ejectScenario.prepareAndRun(() =>
+        runMockThought('eject', 'template').then(result => {
+          expect(result.code).to.equal(0)
+          expect(result.stdout).not.to.match(/partial howitworks\.md\.hbs/)
+          expect(result.stdout).to.match(/template README\.md\.hbs/)
+          expect(result.stderr).to.equal('')
+        })
+      )
+    })
+
+    it('should show only ejectable partials, if the prefix is "partials"', async function() {
+      await ejectScenario.prepareAndRun(() =>
+        runMockThought('eject', 'partial').then(result => {
+          expect(result.code).to.equal(0)
+          expect(result.stdout).to.match(/partial howitworks\.md\.hbs/)
+          expect(result.stdout).not.to.match(/template README\.md\.hbs/)
+          expect(result.stderr).to.equal('')
+        })
+      )
+    })
+
+    it('should eject a partial if a filename is specified and the prefix is "partial"', async function() {
+      await ejectScenario.prepareAndRun(async () => {
+        const result = await runMockThought('eject', 'partial', 'howitworks.md.hbs')
+
+        expect(result.code).to.equal(0)
+        expect(result.stdout).to.match(/Ejecting "\.thought.partials.howitworks\.md\.hbs"/)
+
+        const ejectedPath = path.join('.thought', 'partials', 'howitworks.md.hbs')
+        const ejectedFileContents = await ejectScenario.readActual(ejectedPath)
+
+        const defaultPath = require.resolve('../handlebars/partials/howitworks.md.hbs')
+        const defaultFileContents = await fs.readFile(defaultPath, 'utf-8')
+
+        expect(ejectedFileContents).to.equal(defaultFileContents)
+      })
+    })
+
+    it('should eject a template if a filename is specified and the prefix is "template"', async function() {
+      await ejectScenario.prepareAndRun(async () => {
+        const result = await runMockThought('eject', 'template', 'README.md.hbs')
+
+        expect(result.code).to.equal(0)
+        expect(result.stdout).to.match(/Ejecting "\.thought.templates.README\.md\.hbs"/)
+        expect(result.stderr).to.equal('')
+
+        const ejectedPath = path.join('.thought', 'templates', 'README.md.hbs')
+        const ejectedFileContents = await ejectScenario.readActual(ejectedPath)
+
+        const defaultPath = require.resolve('../handlebars/templates/README.md.hbs')
+        const defaultFileContents = await fs.readFile(defaultPath, 'utf-8')
+
+        expect(ejectedFileContents).to.equal(defaultFileContents)
+      })
+    })
+
+    it('should give sensible error messages', async function() {
+      await ejectScenario.prepareAndRun(async () => {
+        await expectError({ args: ['eject', 'temp'], errorMessage: 'Unknown prefix "temp", try without prefix!' })
+
+        await expectError({
+          args: ['eject', 'template', 'RED.md.hbs'],
+          errorMessage: 'There is no template "RED.md.hbs"!'
+        })
+        await expectError({
+          args: ['eject', 'template', 'anotherFile.md.hbs'],
+          errorMessage: `File ".thought/templates/anotherFile.md.hbs" already exists in this project!`
+        })
+
+        await expectError({
+          args: ['eject', 'partial', 'RED.md.hbs'],
+          errorMessage: 'There is no partial "RED.md.hbs"!'
+        })
+        await expectError({
+          args: ['eject', 'partial', 'usage.md.hbs'],
+          errorMessage: `File ".thought/partials/usage.md.hbs" already exists in this project!`
+        })
+      })
+    })
+
+    async function expectError({ args, errorMessage }) {
+      const result = await runMockThought(...args)
+      expect(result.stderr).to.contain(errorMessage)
+      expect(result.code).to.equal(1)
+    }
   })
 })
 
---
+++ test/fixtures/scenarios/with-partial-and-template/expected/README.md
@@ -0,0 +1,27 @@
+# simple-project 
+
+[![NPM version](https://img.shields.io/npm/v/simple-project.svg)](https://npmjs.com/package/simple-project)
+
+> A simple description
+
+
+# Installation
+
+```
+npm install simple-project
+```
+
+Overridden usage file
+
+
+# License
+
+`simple-project` is published under the ISC-license.
+
+No file "LICENSE*" found
+
+
+ 
+# Contributing guidelines
+
+See [CONTRIBUTING.md](CONTRIBUTING.md).
